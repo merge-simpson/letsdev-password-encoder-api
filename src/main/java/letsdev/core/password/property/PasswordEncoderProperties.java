f0fb7b9d16c883f93f8c534c0484be895fbb7e6d
merge-simpson <letsdev@kakao.com>
2024-10-09 16:26:36 +0900

    feat(auto configuration): Add default encoder field to password encoder properties

--- src/main/java/letsdev/core/password/property/PasswordEncoderProperties.java
+++ src/main/java/letsdev/core/password/property/PasswordEncoderProperties.java
@@ -1,5 +1,7 @@
 package letsdev.core.password.property;
 
+import letsdev.core.password.encoder.GeneralPasswordEncoderType;
+import letsdev.core.password.encoder.PasswordEncoderType;
 import letsdev.core.password.property.argon2.PasswordEncoderArgon2Properties;
 import letsdev.core.password.property.bcrypt.PasswordEncoderBcryptProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,12 +11,16 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 @ConfigurationProperties("letsdev.password")
 @ConfigurationPropertiesBinding
 public record PasswordEncoderProperties(
+        PasswordEncoderType defaultEncoder,
         @NestedConfigurationProperty
         PasswordEncoderBcryptProperties bcrypt,
         @NestedConfigurationProperty
         PasswordEncoderArgon2Properties argon2
 ) {
     public PasswordEncoderProperties {
+        if (defaultEncoder == null) {
+            defaultEncoder = GeneralPasswordEncoderType.BCRYPT;
+        }
         if (bcrypt == null) {
             bcrypt = PasswordEncoderBcryptProperties.defaultInstance();
         }
